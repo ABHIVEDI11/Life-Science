769d380031aef5c771cb5deace31451d771010d2
ABHISHEK DWIVEDI <abhishekdwivedi7754@gmail.com>
2024-10-09 23:41:44 +0530

    01 mol_wt_pdb.py

--- 01 mol_wt_pdb.py
+++ 01 mol_wt_pdb.py
@@ -6,15 +6,15 @@ for i in range (0,len(pdbfile)):
 	# print (line[13:14])
 	if line[13:14] == 'N':
 	   molwt = molwt + 14
-	# elif(line[13:14] == 'O'):
-	#      molwt = molwt + 16
-	# elif(line[13:14] == 'C'):
-	#     molwt = molwt + 12
-	# elif(line[13:14] == 'S'):
-	#      molwt == molwt + 32
-	# elif(line[13:14] == 'H'):
-	#      molwt = molwt + 1
-	# else:
-	#    next
+	elif(line[13:14] == 'O'):
+	     molwt = molwt + 16
+	elif(line[13:14] == 'C'):
+	     molwt = molwt + 12
+ elif(line[13:14] == 'S'):
+	     molwt == molwt + 32
+	elif(line[13:14] == 'H'):
+	     molwt = molwt + 1
+	else:
+	     next
 
 print(f"The molecular weight of this protein is :",molwt)
